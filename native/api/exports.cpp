f22bd6d829d3a496524be2b006bab437d62356ab
Evgeny Kuzmin <Evgeny.Kuzmin@jetbrains.com>
2024-03-27 16:46:09 +0100

    Switch to error messages

--- native/api/exports.cpp
+++ native/api/exports.cpp
@@ -5,23 +5,18 @@
 using namespace events;
 using namespace events::etw;
 
-#pragma pack(push, 1)
-struct error_info {
-    int32_t code;
-    const char * message;
-
-    alignas(std::string) const std::string priv_message;
-
-    error_info(int32_t c, std::string m) noexcept : code(c), message(nullptr), priv_message(std::move(m)) {
-        message = priv_message.c_str();
-    }
-};
-#pragma pack(pop)
-
 template <class T>
-bool check_result(const result_t<T> & result, error_info ** error) {
-    if(const error_t * err = result.try_get_error()) {
-        *error = new error_info{ err->value.value(), err->message };
+bool check_result(const result_t<T> & result, char ** error) noexcept {
+    try {
+        if(const error_t * err = result.try_get_error()) {
+            std::string str = std::string(err->message) + ": " + err->value.message();
+            char * message = new char[str.size() + 1];
+            std::memcpy(message, str.c_str(), str.size() + 1);
+            return false;
+        }
+    }
+    catch(...) {
+        *error = nullptr;
         return false;
     }
     return true;
@@ -32,9 +27,9 @@ bool check_result(const result_t<T> & result, error_info ** error) {
 /////////////
 
 extern "C" __declspec(dllexport)
-void ReleaseErrorInfo(error_info * error)
+void ReleaseString(char * error)
 {
-    delete error;
+    delete [] error;
 }
 
 using OnProviderEnableCallback = void(*)(bool isEnabled, uint64_t matchAnyKeyword, uint64_t matchAllKeyword);
@@ -45,20 +40,20 @@ void __stdcall OnProviderEnableTrampoline(bool isEnabled, uint64_t matchAnyKeywo
 }
 
 extern "C" __declspec(dllexport)
-uint64_t __cdecl RegisterProvider(GUID providerGuid, uint64_t onEnableCallback, error_info ** error)
+uint64_t __cdecl RegisterProvider(GUID providerGuid, uint64_t onEnableCallback, char ** error)
 {
     result_t<uint64_t> handle = register_provider<&OnProviderEnableTrampoline>(providerGuid, reinterpret_cast<void *>(onEnableCallback));
     return check_result(handle, error) ? handle : 0;
 }
 
 extern "C" __declspec(dllexport)
-bool __cdecl UnregisterProvider(uint64_t providerHandle, error_info ** error)
+bool __cdecl UnregisterProvider(uint64_t providerHandle, char ** error)
 {
     return check_result(unregister_provider(providerHandle), error);
 }
 
 extern "C" __declspec(dllexport)
-bool WriteDebugOutput(uint64_t providerHandle, const char * str, error_info ** error)
+bool WriteDebugOutput(uint64_t providerHandle, const char * str, char ** error)
 {
     using dbg_output = JetBrains_Common_DebugOutput::DebugOutput;
     return check_result(write_event(providerHandle, { dbg_output::id, dbg_output::version }, dbg_output::data::to_tuple({ static_cast<uint32_t>(GetCurrentProcessId()), str })), error);
